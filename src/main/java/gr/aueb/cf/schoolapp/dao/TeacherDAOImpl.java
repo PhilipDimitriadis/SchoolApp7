2ac2dfcf0cca4a9a7cfc3bd5bae83131e5cfb7f8
Philip Dimitriadis <philipdimitriadis@gmail.com>
2025-04-24 15:30:28 +0300

    feat: DTO layer created

--- src/main/java/gr/aueb/cf/schoolapp/dao/TeacherDAOImpl.java
+++ src/main/java/gr/aueb/cf/schoolapp/dao/TeacherDAOImpl.java
@@ -8,6 +8,7 @@ import java.sql.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 public class TeacherDAOImpl implements ITeacherDAO {
 
@@ -32,7 +33,7 @@ public class TeacherDAOImpl implements ITeacherDAO {
             ps.setString(8, teacher.getStreetNum());
             ps.setString(9, teacher.getZipCode());
             ps.setInt(10, teacher.getCityId());
-            ps.setString(11, teacher.getUuid());
+            ps.setString(11, UUID.randomUUID().toString());
             ps.setTimestamp(12, Timestamp.valueOf(LocalDateTime.now()));
             ps.setTimestamp(13, Timestamp.valueOf(LocalDateTime.now()));
 
@@ -237,7 +238,7 @@ public class TeacherDAOImpl implements ITeacherDAO {
             }
             return teacher;
         } catch (SQLException e) {
-            e.printStackTrace();
+//            e.printStackTrace();
             // logging
             throw new TeacherDAOException("SQL Error. Teacher with id: " + vat + " error while searching.");
         }
